1197e3f37074fc2103156401480ed736daac3217
daultonoryan <31106349+daultonoryan@users.noreply.github.com>
2019-07-19 15:17:26 -0700

    Update part1.js

--- FirstGameTutorial/part1.js
+++ FirstGameTutorial/part1.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
